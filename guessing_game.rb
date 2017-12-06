ce034819554ce4d54ff8616afa4cb6ed51797b13
Michael Glaser <m.glaser82@gmail.com>
2017-12-06 13:39:45 -0800

    Refactored some of guessing_game.rb to fit the ruby style guide.

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,26 +1,27 @@
 class Guessing_game
   VALID_Numbers = (1..100).to_a # Store valid answers in an array
 
-  def initialize answer; @answer = answer
-@solved = false
-# Validate input
-  raise "Answer must be between 1 and 100" unless VALID_Numbers.include? @answer
+  def initialize(answer)
+    @answer = answer
+    @solved = false
+    # Validate input
+    raise "Answer must be between 1 and 100" unless VALID_Numbers.include? @answer
   end
 
-def guess ( number )
-if number == @answer # Check if the two are equal
-@solved = true
+  def guess ( number )
+    if number == @answer # Check if the two are equal
+      @solved = true
       :correct
-  elsif (number > @answer) # Check if the guess is higher
-@solved = false
-    return :high
-elsif(number<@answer) # Check if the guess is lower
+    elsif (number > @answer) # Check if the guess is higher
       @solved = false
-    :low
-end
+      return :high
+    elsif(number<@answer) # Check if the guess is lower
+      @solved = false
+      :low
+    end
   end
 
   def solved?
-  @solved
-end
+    @solved
+  end
 end
